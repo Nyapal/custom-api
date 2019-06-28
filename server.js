90ce297eb904cc862782eb50da9a475542acc40d
nyapal <nyapal14@gmail.com>
2019-06-28 00:00:42 -0700

    users can sign up

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
+require('dotenv').config();
 const express = require('express')
 const exphbs  = require('express-handlebars');
 const bodyParser = require('body-parser')
 const expressValidator = require('express-validator')
+const cookieParser = require('cookie-parser')
+const jwt = require('jsonwebtoken')
 const app = express()
 const port = 3000
 
 // Use Body Parser
-// const jsonParser = bodyParser.json()
-// const urlencodedParser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -17,7 +18,9 @@ app.use( expressValidator());
 app.engine('.hbs', exphbs({extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
-require('./controllers/plants.js')(app)
+app.use(cookieParser())
+require('./controllers/plants.js')(app);
+require('./controllers/auth.js')(app);
 require('./data/plantparty-db');
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
